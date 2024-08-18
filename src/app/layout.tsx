0e8dc89ebc127611997f42916b43eef112f84ffd
Dwi Yulianto <82428562+docd-dev@users.noreply.github.com>
2024-08-18 11:05:46 +0700

    add layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("", inter.className)}>{children}</body>
+      <body className={cn("bg-neutral-700", inter.className)}>{children}</body>
     </html>
   );
 }
